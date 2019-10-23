4ab2445df13a358873d46c1440d6fb8ed27d894c
takashi365 <yuda365@gmail.com>
2019-10-24 02:11:24 +0900

    管理者権限を実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -17,6 +17,12 @@ class ShopsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    shop = Shop.find(params[:id])
+    shop.destroy
+    redirect_to root_path
+  end
+
   private
   def shop_params
     params.require(:shop).permit(:name, :image, :address, :tel)
