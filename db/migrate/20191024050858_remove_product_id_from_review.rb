c77a303dd2a392a9d357bf9a0f25ad02fa0cc05c
takashi365 <yuda365@gmail.com>
2019-12-04 16:06:31 +0900

    デプロイ周り

--- db/migrate/20191024050858_remove_product_id_from_review.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductIdFromReview < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :reviews, :product_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_050858) do
+ActiveRecord::Schema.define(version: 2019_10_24_050500) do
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "rate"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_050858) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "shop_id"
+    t.integer "product_id"
   end
 
   create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
